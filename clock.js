e7ccb546c939ada2f4b7e2d4c0d1147264f00263
Victoria Kee <victoriakee95@gmail.com>
2020-05-07 10:55:32 -0500

    Draw the hands of the clock function

--- clock.js
+++ clock.js
@@ -94,3 +94,14 @@ function drawTime(ctx, radius){
   second = (second*Math.PI/30);
   drawHand(ctx, second, radius*0.9, radius*0.02);
 };
+
+function drawHand(ctx, pos, length, width){
+  ctx.beginPath();
+  ctx.lineWdith = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0, 0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
+};
